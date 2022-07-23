817b93cb2acf8548537d1359013cb2b7356b1321
Homework1 <danisholha760@gmail.com>
2022-07-22 23:56:33 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,118 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+// Task 1
+
+        // 1
+        int[] weights = new int[3];
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] = (i * 1) + 1;
+            System.out.println(weights[i]);
+        }
+        System.out.println();
+
+        // 2
+        double[] boxes = {1.57, 7.654, 9.986};
+        for (int i = 0; i < boxes.length; i++) {
+            System.out.println(boxes[i]);
+        }
+        System.out.println();
+
+        // 3
+        int[] weeks = {18, 20, 9, 70, 85, 12};
+        for (int i = 0; i < weeks.length; i++) {
+            System.out.println(weeks[i]);
+        }
+        System.out.println();
+
+// Task 2
+
+        // 1
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] = (i * 1) + 1;
+            if (i == weights.length - 1) {
+                System.out.print(weights[i]);
+            } else {
+                System.out.print(weights[i] + ", ");
+            }
+        }
+        System.out.println();
+
+        // 2
+        for (int i = 0; i < boxes.length; i++) {
+            if (i == boxes.length - 1) {
+                System.out.print(boxes[i]);
+            } else {
+                System.out.print(boxes[i] + ", ");
+            }
+        }
+        System.out.println();
+
+        // 3
+        for (int i = 0; i < weeks.length; i++) {
+            if (i == weeks.length - 1) {
+                System.out.print(weeks[i]);
+            } else {
+                System.out.print(weeks[i] + ", ");
+            }
+        }
+        System.out.println();
+        System.out.println();
+
+// Task 3
+
+        // 1
+        for (int i = weights.length - 1; i >= 0; i--) {
+            if (i == weights.length - weights.length) {
+                System.out.print(weights[i]);
+            } else {
+                System.out.print(weights[i] + ", ");
+            }
+        }
+        System.out.println();
+
+        // 2
+        for (int i = boxes.length - 1; i >= 0; i--) {
+            if (i == boxes.length - boxes.length) {
+                System.out.print(boxes[i]);
+            } else {
+                System.out.print(boxes[i] + ", ");
+            }
+        }
+        System.out.println();
+
+        // 3
+        for (int i = weeks.length - 1; i >= 0; i--) {
+            if (i == weeks.length - weeks.length) {
+                System.out.print(weeks[i]);
+            } else {
+                System.out.print(weeks[i] + ", ");
+            }
+        }
+        System.out.println();
+        System.out.println();
+
+
+// Task 4
+
+        // 1
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < weeks.length; i ++) {
+            list.add(weeks[i]);
+        }
+        ArrayList<Integer> odd = new ArrayList<Integer>();
+
+        for (int i = 0; i < list.size(); i++) {
+            int x = list.get(i);
+            if (x % 2 != 0)
+                odd.add(x + 1);
+        }
+        System.out.println(odd);
+
+
+
     }
 }
